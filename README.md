f6a022d10d3b786b421ab6c8937b0ade79979649
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-06-26 22:57:58 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Quote of the Day
+
+
+### Click below to view live demo
+[Click Here](https://epic-heyrovsky-822274.netlify.app/)
---
+++ main.js
@@ -0,0 +1,21 @@
+//variables
+
+const btn = document.getElementById('btn');
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+
+let url = "http://quotes.rest/qod.json";
+
+
+
+getData();
+
+async function getData(){
+    const response = await fetch(url);
+    const data = await response.json();
+
+    quote.textContent = data.contents.quotes[0].quote;
+    author.textContent = data.contents.quotes[0].author;
+}
+    
+    
